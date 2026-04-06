4896bffc27500948bfa205bbfc89c45f23ad6af4
0715431007-hue <0715431007@s.tusd1.org>
2026-04-06 15:47:06 +0000

    commit and push

--- script.js
+++ script.js
@@ -4,20 +4,60 @@ function start(){
     console.log("click");
 
     //start level 1
-    createCircle();
+    let num = getRandomNumber();
+    createCircle(num);
+    createCircle(num);
+
+    num = getRandomNumber();
+    createCircle(num);
+    createCircle(num);
+
+    num = getRandomNumber();
+    createCircle(num);
+    createCircle(num);
+
+    num = getRandomNumber();
+    createCircle(num);
+    createCircle(num);
 }
 
-function createCircle() {
+function createCircle(num) {
     //create div for circle
     let circle = document.createElement("div");
 
     //set text to a number
-    circle.innerText = "1";
+    circle.innerText = num;
 
     //set color/shape/font/etc
     circle.style.backgroundColor = "white";
     circle.style.fontSize = "25px";
+    circle.style.borderRadius = "30px";
+    circle.style.alignContent = "center";
+    circle.style.textAlign = "center";
+    circle.style.height = "60px";
+    circle.style.width = "60px";
+    circle.style.position = "absolute";
+
+    circle.style.left = getRandomX() + "px";
+    circle.style.top = getRandomY() + "px";
 
     //add circle to page
     document.body.appendChild(circle);
+}
+
+//generates random x coordinates
+function getRandomX() {
+    let randomX = Math.random() * window.innerWidth;
+    return randomX;
+}
+
+function getRandomY() {
+    let randomY = Math.random() * window.innerHeight;
+    return randomY;
+}
+
+function getRandomNumber() {
+    let random = Math.random() * 100;
+    random = Math.floor(random);
+    return random;
 }
